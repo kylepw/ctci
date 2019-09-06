2543a2e323f989564c211778d208c0bdc0891cab
Kyle <kylepw@gmail.com>
2019-09-06 22:38:37 +0900

    Todo: add another method for q13

--- q13.py
+++ q13.py
@@ -1,6 +1,7 @@
 """
     q13.py
     ~~~~~~
+
     Write a method to replace all spaces in a string with '%20'. You may
     assume that the string has sufficient space at the end to hold the
     additional characters, and that you are given the "true" length of the
@@ -13,28 +14,34 @@
 """
 import unittest
 
-
 def urlify(s, length):
-    SPACE = '%20'
-
-    chars = string.split()
-
-    replaced = ['%20' for c in chars if c == ' ' else c]
-    print(replaced)
+    pass
 
 
-
+def urlify_simple(s, length):
+    return s[:length].replace(' ', '%20')
 
 class TestUrlify(unittest.TestCase):
     def setUp(self):
-        params = [
-            {('John Foody Weiner   ', 17): 'John%20Foody%20Weiner'},
-            {('I  like   TKG  ', 13): 'I%20%20like%20%20%20TKG'},
-            {('  What  is  100 cm? ', 19): '%20%20What%20%20is%20%20100%20cm?'},
-            {('value="I   like turtles"', 23): 'value="I%20%20%20like%20turtles'},
-            {('    ', 3): '%20%20%20'},
-        ]
+        self.args = {
+            ('much ado about nothing      ', 22): 'much%20ado%20about%20nothing',
+            ('Mr John Smith    ', 13): 'Mr%20John%20Smith',
+        }
 
     def test_urlify(self):
-        for p, expected in params.items():
-            self.assertEqual(urlify(*p), expected)
+        for arg, expected in self.args.items():
+            self.assertEqual(urlify(*arg), expected)
+
+class TestUrlifySimple(unittest.TestCase):
+    def setUp(self):
+        self.args = {
+            #('John Foody Weiner   ', 17): 'John%20Foody%20Weiner',
+            ('I  like   TKG  ', 13): 'I%20%20like%20%20%20TKG',
+            ('  What  is  100 cm? ', 19): '%20%20What%20%20is%20%20100%20cm?',
+            ('value="I   like turtles"', 23): 'value="I%20%20%20like%20turtles',
+            ('    ', 3): '%20%20%20',
+        }
+
+    def test_urlify(self):
+        for arg, expected in self.args.items():
+            self.assertEqual(urlify_simple(*arg), expected)
