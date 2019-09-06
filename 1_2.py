3947a3d11d0eca70efa376fbad9462bc36a16f6e
Kyle <kylepw@gmail.com>
2019-09-06 17:15:02 +0900

    Add O(N) version

--- 1_2.py
+++ 1_2.py
@@ -7,10 +7,12 @@
 """
 import unittest
 
+
 def sort_str(s):
     return ''.join(sorted(s))
 
 def is_permutation(str1, str2):
+    """Runtime: O(n^2)?"""
     if not isinstance(str1, str) or not isinstance(str2, str):
         return False
     if len(str1) < 1 or len(str2) < 1:
@@ -20,15 +22,11 @@ def is_permutation(str1, str2):
 
     str1 = sort_str(str1)
     str2 = sort_str(str2)
-    print(str1, str2)
 
     window_pos = 0
     found = False
     for c1 in str1:
-        print('Window position: ', window_pos)
-        print(c1)
         for i, c2 in enumerate(str2[window_pos:]):
-            print(i, c2)
             if c1 == c2:
                 found = True
                 window_pos += i + 1
@@ -39,7 +37,8 @@ def is_permutation(str1, str2):
             found = False
     return True
 
-def is_permutation_pythonic(str1, str2):
+def is_permutation_dict(str1, str2):
+    """Runtime: O(N)"""
     if not isinstance(str1, str) or not isinstance(str2, str):
         return False
     if len(str1) < 1 or len(str2) < 1:
@@ -47,38 +46,49 @@ def is_permutation_pythonic(str1, str2):
     if len(str1) > len(str2):
         return False
 
-    str1 = sort_str(str1)
-    str2 = sort_str(str2)
-
-    window_pos = 0
-    for c1 in str1:
-        for i, _ in enumerate(str2[window_pos:]):
-            if c1 in str2[window_pos:]:
-                window_pos += i
-                break
-            else:
-                return False
+    count1 = {}
+    count2 = {}
+    for c in str1:
+        if c in count1:
+            count1[c] += 1
+        else:
+            count1[c] = 1
+    for c in str2:
+        if c in count2:
+            count2[c] += 1
+        else:
+            count2[c] = 1
+    for c in str1:
+        if c not in count2 or count1[c] > count2[c]:
+            return False
     return True
-
 
 
 class TestPermutation(unittest.TestCase):
     def setUp(self):
-        self.str1 = 'cba'
-        self.str2 = 'blahcgah'
-
-        self.str3 = 'aaa'
-        self.str4 = 'zeopa'
+        self.true_vals = [
+            ('cba', 'blahcgah'),
+            ('aaa', 'zaeopa4a'),
+            ('4329', '5489203'),
+            ('!!$$5jlp', '!5lk!jp4l$.$')
+        ]
+        self.false_vals = [
+            ('hey', 'he'),
+            (123, 'yo'),
+            ('', ''),
+            ('kul', 'kkkkkk'),
+            ('uer', 'erqqqd'),
+            ('oooops', 'psooohey')
+        ]
 
     def test_permutation(self):
-        self.assertTrue(is_permutation(self.str1, self.str2))
-        self.assertFalse(is_permutation(self.str3, self.str4))
+        for (str1, str2) in self.true_vals:
+            self.assertTrue(is_permutation(str1, str2))
+        for (str1, str2) in self.false_vals:
+            self.assertFalse(is_permutation(str1, str2))
 
-    @unittest.SkipTest
-    def test_permutation_pythonic(self):
-        self.assertTrue(is_permutation_pythonic(self.str1, self.str2))
-        #self.assertFalse(is_permutation_pythonic(self.str3, self.str4))
-
-
-
-
+    def test_permutation_dict(self):
+        for (str1, str2) in self.true_vals:
+            self.assertTrue(is_permutation_dict(str1, str2))
+        for (str1, str2) in self.false_vals:
+            self.assertFalse(is_permutation_dict(str1, str2))
