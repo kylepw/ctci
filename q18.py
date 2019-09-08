5c694860f222290604ace1ec6cacc0c9303543f5
Kyle <kylepw@gmail.com>
2019-09-08 21:32:28 +0900

    Finish q18

---
+++ q18.py
@@ -0,0 +1,25 @@
+"""
+    q18.py
+    ~~~~~~
+
+    Zero Matrix: Write an algorithm such that if an element in an MxN matrix
+    is 0, its entire row and column are set to 0.
+
+    Hints: #17, 74, #102
+"""
+import unittest
+
+
+def method():
+    pass
+
+class TestMethod(unittest.TestCase):
+    def setUp(self):
+        self.data = ()
+
+    def test_method(self):
+        for d in self.data:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
