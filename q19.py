80fe86fd3b82a1144d13b42e7f2b68b9cac6461e
Kyle <kylepw@gmail.com>
2019-09-08 21:32:42 +0900

    Add q19 template

---
+++ q19.py
@@ -0,0 +1,27 @@
+"""
+    q19.py
+    ~~~~~~
+
+    String Rotation: Assume you have a method isSubstring which checks if one
+    word is a substring of another. Given two strings, s1 and s2, write code
+    to check if s2 is a rotation of s1 using only one call to isSubstring
+    (e.g., "waterbottle" is a rotation of "erbottlewat").
+
+    Hints: #34, #88, #104
+"""
+import unittest
+
+
+def method():
+    pass
+
+class TestMethod(unittest.TestCase):
+    def setUp(self):
+        self.data = ()
+
+    def test_method(self):
+        for d in self.data:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
