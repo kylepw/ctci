21d0453c7ed671a828f43979ce4ba1b0671186dc
Kyle <kylepw@gmail.com>
2019-09-09 15:12:08 +0900

    Complete q18

--- q18.py
+++ q18.py
@@ -10,16 +10,51 @@
 import unittest
 
 
-def method():
-    pass
+def zero_matrix(matrix):
+    M = len(matrix)
+    N = len(matrix[0])
 
-class TestMethod(unittest.TestCase):
-    def setUp(self):
-        self.data = ()
+    columns = set()
+    for i in range(M):
+        for j in range(N):
+            if matrix[i][j] == 0:
+                zero_row(matrix, row=i)
+                columns.add(j)
+                break
+    zero_columns(matrix, columns)
+    return matrix
 
-    def test_method(self):
-        for d in self.data:
-            pass
+def zero_row(matrix, row):
+    for j in range(len(matrix[0])):
+        matrix[row][j] = 0
 
-if __name__ == '__main__':
+def zero_columns(matrix, columns):
+    for column in columns:
+        for i in range(len(matrix)):
+            matrix[i][column] = 0
+
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ([
+            [1, 2, 3, 4, 0],
+            [6, 0, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 0, 18, 19, 20],
+            [21, 22, 23, 24, 25]
+        ], [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [11, 0, 13, 14, 0],
+            [0, 0, 0, 0, 0],
+            [21, 0, 23, 24, 0]
+        ])
+    ]
+
+    def test_zero_matrix(self):
+        for [test_matrix, expected] in self.data:
+            actual = zero_matrix(test_matrix)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
     unittest.main()
