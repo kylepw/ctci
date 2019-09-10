e5b6ae58bbfc19717047262158ccbdd2904fff24
Kyle <kylepw@gmail.com>
2019-09-10 22:57:31 +0900

    Complete q22

--- q22.py
+++ q22.py
@@ -2,10 +2,25 @@
     q22.py
     ~~~~~~
 
-    
+    Implement an algorithm to find the kth to last element of a singly
+    linked list.
+
+    Hints: #8, #25, #41, #67, #126
 """
-def kth_to_last(ll):
-    pass
+from linkedlist import LinkedList
+
+def kth_to_last(ll, k):
+    current = runner = ll.head
+
+    # Initalize runner k nodes ahead
+    for i in range(k):
+        runner = runner.next
+
+    while runner:
+        current = current.next
+        runner = runner.next
+    return current.value
+
 
 if __name__ == '__main__':
     ll = LinkedList()
