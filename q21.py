47a948b444c897108eeae44c2fadc5af70dbc646
Kyle <kylepw@gmail.com>
2019-09-10 15:57:19 +0900

    Work on running pointer version of q21

--- q21.py
+++ q21.py
@@ -10,7 +10,10 @@
 """
 
 def remove_dups(head):
-    """Remove duplicate node values from linked list that starts at `head`."""
+    """Remove duplicate node values from linked list that starts at `head`.
+
+        Runtime: O(N)
+    """
     n = head
     vals = set()
     vals.add(n.data)
@@ -23,6 +26,19 @@ def remove_dups(head):
             n = n.next
     return head
 
+def remove_dups_run(head):
+    """Remove duplicate node values with running pointers.
+
+        Runtime: O(N^2)
+    """
+    p1 = p2 = head
+    while (p1.next):
+        p1 = p1.next.next if p1.next.next else p1.next
+        p2 = p2.next
+    # p1 is at beginning, p2 is beginning of second half
+    p1 = head
+    
+
 class Node:
     def __init__(self, data, next=None):
         self.data = data
@@ -32,13 +48,13 @@ class Node:
 
 def print_list(head):
     n = head
-    while n.next != None:
+    while n.next.next:
         print(n, '-> ', end='')
         n = n.next
     print(n)
 
-if __name__ == '__main__':
-    # Initialize values
+def init_list():
+    """Return head of linked list with values"""
     head = Node(data=0)
     n = head
     for data in range(1, 5):
@@ -48,6 +64,18 @@ if __name__ == '__main__':
     n.next = Node(data=0)
     n.next.next = Node(data=2)
 
-    print_list(head)
-    print('After duplicates removed:')
-    print_list(remove_dups(head))
+    return head
+
+if __name__ == '__main__':
+    # Initialize values
+    """ head1 = init_list()
+    print_list(head1)
+    print('After duplicates removed in O(N):')
+    print_list(remove_dups(head1)) """
+
+    head2 = init_list()
+
+    print_list(head2)
+    print('After duplicates removed in O(N^2): ')
+    remove_dups_run(head2)
+    #print_list(remove_dups_run(head2))
