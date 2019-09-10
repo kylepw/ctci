d10dd2cd05f66d5a1e7edaad4e7d65964c6c960d
Kyle <kylepw@gmail.com>
2019-09-10 23:01:12 +0900

    Prepare q23

--- q23.py
+++ q23.py
@@ -1,7 +1,18 @@
 """
     q23.py
     ~~~~~~
+
+    Delete Middle Node: Implement an algorithm to delete a node in the middle
+    (i.e., any node but the first and last node, not necessarily the exact
+    middle) of a singly linked list, given only access to that node.
+
+    Example:
+    Input: the node c from the linked list a->b->c->d->e->f
+    Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+
+    Hints: #72
 """
+from linkedlist import LinkedList
 
 def delete_middle_node(middle_node):
     pass
