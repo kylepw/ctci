52b808ed198356d49e5d50ed88468e18127b43bb
Kyle <kylepw@gmail.com>
2019-09-11 23:32:09 +0900

    Add q26 doc

--- q26.py
+++ q26.py
@@ -1,6 +1,9 @@
 """
     q26.py
     ~~~~~~
+    Palindrome: Implement a function to check if a linked list is a palindrome.
+
+    Hints: #5, #13, #29, #61, #101
 """
 from linkedlist import LinkedList
 
