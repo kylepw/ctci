e2f09f3e1e639362ecc7abeff6162c00c9a205ba
Kyle <kylepw@gmail.com>
2019-09-11 23:27:50 +0900

    Complete q25 followup

--- q25.py
+++ q25.py
@@ -38,18 +38,72 @@ def _int_to_ll(n):
         n = (n - remainder) // 10
     return LinkedList(lst_vals)
 
-def sum_lists(ll_a, ll_b):
+def sum_lists_2(ll_a, ll_b):
+    """Easy conversion implementation"""
     sum = _sum_list(ll_a) + _sum_list(ll_b)
     return f'{_int_to_ll(sum)}'
 
+def sum_lists(ll_a, ll_b):
+    """Better implementation"""
+    ll = LinkedList()
+    num_a = ll_a.head
+    num_b = ll_b.head
+
+    carry = 0
+    while num_a or num_b:
+        sum = carry
+        if num_a:
+            sum += num_a.value
+            num_a = num_a.next
+        if num_b:
+            sum += num_b.value
+            num_b = num_b.next
+
+        ll.add(sum % 10)
+        carry = sum // 10
+    if carry:
+        ll.add(carry)
+    return ll
+
+
 def sum_lists_followup(ll_a, ll_b):
-    pass
+    ll = LinkedList()
+
+    if len(ll_a) != len(ll_b):
+        diff = abs(len(ll_a) - len(ll_b))
+        smaller = ll_a if len(ll_a) < len(ll_b) else ll_b
+        for _ in range(diff):
+            smaller.add_to_beginning(0)
+
+    carry = 0
+    for i in reversed(range(len(ll_a))):
+        pos_a = ll_a.head
+        pos_b = ll_b.head
+        for _ in range(i):
+            pos_a = pos_a.next
+            pos_b = pos_b.next
+        sum = pos_a.value + pos_b.value + carry
+        ll.add_to_beginning(sum % 10)
+        carry = sum // 10
+
+    if carry:
+        ll.add_to_beginning(carry)
+    return ll
 
 if __name__ == '__main__':
     ll_a = LinkedList()
     ll_a.generate(4, 0, 9)
     ll_b = LinkedList()
     ll_b.generate(3, 0, 9)
+    print(ll_a)
+    print(ll_b)
+    print(sum_lists(ll_a, ll_b))
+    print(sum_lists_followup(ll_a, ll_b))
+
+    ll_a = LinkedList()
+    ll_a.add_multiple([9, 7, 8])
+    ll_b = LinkedList()
+    ll_b.add_multiple([6, 8, 5])
     print(ll_a)
     print(ll_b)
     print(sum_lists(ll_a, ll_b))
