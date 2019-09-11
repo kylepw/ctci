25d64141eec44cc2f3e2aca1dc43adc5598ccd6b
Kyle <kylepw@gmail.com>
2019-09-11 15:59:05 +0900

    Complete q25

--- q25.py
+++ q25.py
@@ -1,11 +1,46 @@
 """
     q25.py
     ~~~~~~
+    You have two numbers represented by a linked list, where each node
+    contains a single digit. The digits are stored in reverse order, such
+    that the 1's digit is at the head of the list. Write a function that
+    adds the two numbers and returns the sum as a linked list.
+    Example:
+    Input:  (7 -> 1 -> 6) + (5 -> 9 -> 2). That is, 617 + 295.
+    Output: 2 -> 1 -> 9. That is, 912.
+
+    Follow up:
+    Suppose the digits are stored in forward order. Repeat the above problem.
+    Example:
+    Input:  (6 -> 1 -> 7) + (2 -> 9 -> 5). That is, 617 + 295.
+    Output: 9 -> 1 -> 2. That is, 912.
+
+    Hints: #7, #30, #71, #95, #109
 """
 from linkedlist import LinkedList
 
+def _sum_list(ll):
+    sum = 0
+    p = 1
+    runner = ll.head
+    while runner:
+        sum += runner.value * p
+        p *= 10
+        runner = runner.next
+    return sum
+
+def _int_to_ll(n):
+    """Convert integer to linked list of digits"""
+    lst_vals = []
+    while n:
+        remainder = n % 10
+        lst_vals.append(remainder)
+        n = (n - remainder) // 10
+    return LinkedList(lst_vals)
+
 def sum_lists(ll_a, ll_b):
-    pass
+    sum = _sum_list(ll_a) + _sum_list(ll_b)
+    return f'{_int_to_ll(sum)}'
 
 def sum_lists_followup(ll_a, ll_b):
     pass
