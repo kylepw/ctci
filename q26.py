6c08cd0d2bde34b86e7cf5248488cce4e51d3d3f
Kyle <kylepw@gmail.com>
2019-09-12 12:41:55 +0900

    Complete q26

--- q26.py
+++ q26.py
@@ -8,10 +8,42 @@
 from linkedlist import LinkedList
 
 def is_palindrome(ll):
-    pass
+    stack = []
+
+    runner = ll.head
+    while runner:
+        stack.append(runner.value)
+        runner = runner.next
+
+    runner = ll.head
+    while stack and runner:
+        if stack.pop() != runner.value:
+            return False
+        runner = runner.next
+    return True
+
+def is_palindrome_solution(ll):
+    """Practice given solution"""
+    print(ll)
+    stack = []
+
+    slow = fast = ll.head
+    while fast and fast.next:
+        stack.append(slow.value)
+        slow = slow.next
+        fast = fast.next.next
+    if fast:
+        slow = slow.next
+    while slow:
+        if stack.pop() != slow.value:
+            return False
+        slow = slow.next
+    return True
 
 if __name__ == '__main__':
     ll_true = LinkedList([1, 2, 3, 4, 5, 4, 3, 2, 1])
-    print(is_palindrome(ll_true))
+    #print(is_palindrome(ll_true))
+    print(is_palindrome_solution(ll_true))
     ll_false = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    print(is_palindrome(ll_false))
+    #print(is_palindrome(ll_false))
+    print(is_palindrome_solution(ll_false))
