a681e3878d773704849d8f641135d6c5cf6e3d37
Kyle <kylepw@gmail.com>
2019-09-12 22:46:59 +0900

    Complete q28

--- q28.py
+++ q28.py
@@ -12,10 +12,36 @@
 
     Hints: #50, #69, #83, #90
 """
-from linkedlist import LinkedList
+from linkedlist import LinkedList, LinkedListNode
 
 def loop_detection(ll):
-    pass
+    if ll.tail.next is None:
+        return
+
+    current = ll.head
+    runner = ll.head.next.next
+    while current is not runner:
+        current = current.next.next
+        runner = runner.next
+    return current
+
+def loop_detection_pythonic(ll):
+    if ll.tail.next is None:
+        return
+
+    stack = []
+    node = ll.head
+    while node not in stack:
+        stack.append(node)
+        node = node.next
+    return node
 
 if __name__ == '__main__':
-    pass
+    loop_node = LinkedListNode(3)
+    ll = LinkedList([1, 2, 0])
+    ll.tail.next = loop_node
+    ll.tail = ll.tail.next
+    ll.add(4)
+    ll.tail.next = loop_node
+    ll.tail = ll.tail.next
+    print(loop_detection(ll))
