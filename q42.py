2a902b7a7916766f8e83f5bd49074466e5d97827
Kyle <kylepw@gmail.com>
2019-10-03 08:53:35 +0900

    Add q42 template and solution

---
+++ q42.py
@@ -0,0 +1,28 @@
+"""
+    q42.py
+    ~~~~~~
+    Minimal Tree: Given a sorted (increasing order) array with unique integer
+    elements, write an algorithm to create a binary search tree with minimal
+    height.
+
+    Hints: #19, #73, #116
+"""
+import unittest
+
+
+def create_bst(array):
+    """Create binary search tree from sorted `array`."""
+    pass
+
+
+class TestCreateBst(unittest.TestCase):
+    def setUp(self):
+        self.data = ()
+
+    def test_create_bst(self):
+        for d in self.data:
+            pass
+
+
+if __name__ == '__main__':
+    unittest.main()
