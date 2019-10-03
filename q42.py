d433a10238cd080b7dbecbf10831f3a490fc5227
Kyle <kylepw@gmail.com>
2019-10-03 09:36:30 +0900

    Split values for binary tree

--- q42.py
+++ q42.py
@@ -10,16 +10,29 @@
 import unittest
 
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+
 def create_bst(array):
     """Create binary search tree from sorted `array`."""
-    pass
+    if array != sorted(array):
+        return False
+    root = array[len(array) // 2]
+    left = [val for val in array if val <= root]
+    right = [val for val in array if val > root]
+    print(array)
+    print(root, left, right)
+
 
 
 class TestCreateBst(unittest.TestCase):
     def setUp(self):
         self.data = {
-            '?': [3, 5, 6, 7, 8, 23, 34, 54, 61],
-            '??': [0, 2, 5, 6, 11, 45, 101, 238],
+            '?': [3, 5, 6, 9, 17, 23],
+            #'??': [0, 2, 5, 6, 11, 45, 101],
         }
 
     def test_create_bst(self):
