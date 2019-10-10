1aeb0fd35bbc8837bed22686a3c1272bdc639090
Kyle <kylepw@gmail.com>
2019-10-10 12:00:14 +0900

    Add q43

--- q42.py
+++ q42.py
@@ -16,28 +16,60 @@ class Node:
         self.children = []
 
 
+    def __str__(self):
+        return f'Node ({self.value})'
+
+def _insert_vals(head, values):
+    """Insert binary tree values in `values` from `head`"""
+    stack = [head]
+
+    while stack:
+        current = stack.pop()
+        while len(current.children) < 2 and values:
+            current.children.append(Node(values.pop()))
+        stack.extend(current.children)
+
+    return head
+
 def create_bst(array):
     """Create binary search tree from sorted `array`."""
     if array != sorted(array):
         return False
-    root = array[len(array) // 2]
-    left = [val for val in array if val <= root]
-    right = [val for val in array if val > root]
-    print(array)
-    print(root, left, right)
+    root_val = array.pop(len(array) // 2)
+    left = [val for val in array if val <= root_val]
+    right = [val for val in array if val > root_val]
+
+    root = Node(root_val)
+    left_head = Node(left.pop())
+    right_head = Node(right.pop())
+    root.children.extend([left_head, right_head])
+
+    _insert_vals(left_head, left)
+    _insert_vals(right_head, right)
+
+    return root
 
 
 
 class TestCreateBst(unittest.TestCase):
     def setUp(self):
-        self.data = {
-            '?': [3, 5, 6, 9, 17, 23],
-            #'??': [0, 2, 5, 6, 11, 45, 101],
-        }
+        self.arrays = (
+            [3, 5, 6, 9, 17, 23],
+            [0, 2, 5, 6, 11, 45, 101],
+        )
 
     def test_create_bst(self):
-        for tree, array in self.data.items():
-            self.assertEqual(create_bst(array), tree)
+        for array in self.arrays:
+            root = create_bst(array)
+            stack = [root]
+            print(f'Min val: {root.value}')
+            while stack:
+                node = stack.pop()
+                if node.value <= root.value:
+                    print('left', node)
+                else:
+                    print('right', node)
+                stack.extend(node.children)
 
 
 if __name__ == '__main__':
---
+++ q43.py
@@ -0,0 +1,84 @@
+"""
+    q43_re.py
+    ~~~~~~
+    List of Depths: Given a binary tree, design an algorithm which creates
+    a linked list of all the nodes at each depth (e.g. if you have a tree
+    with depth D, you'll have D linked lists).
+
+    Hints: #107, #123, #135
+"""
+from math import log
+import unittest
+
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def __repr__(self):
+        return f'Node ({self.value})'
+
+
+class LinkedListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        if self.next is None:
+            return f'Node ({self.value})'
+        return f'Node ({self.value}) -> {self.next}'
+
+
+def array_to_binary(array, start=None, end=None):
+    """Create binary search tree from `array` values via recursion."""
+    start = 0 if start is None else start
+    end = len(array) - 1 if end is None else end
+    if start > end:
+        return ''
+    mid = (start + end) // 2
+    node = Node(array[mid])
+    node.left = array_to_binary(array, start, mid - 1)
+    node.right = array_to_binary(array, mid + 1, end)
+
+    return node
+
+
+def list_of_depths(node, depth_nodes=None, depth=None):
+    """Return list of nodes at each depth."""
+    if not node:
+        return
+    if depth_nodes is None:
+        depth_nodes = []
+    if depth is None:
+        depth = 0
+
+    if len(depth_nodes) - 1 < depth:
+        depth_nodes.append(LinkedListNode(node.value))
+    else:
+        depth_nodes[depth].next = LinkedListNode(node.value)
+    list_of_depths(node.left, depth_nodes, depth + 1)
+    list_of_depths(node.right, depth_nodes, depth + 1)
+
+    return node
+
+
+class TestListOfDepths(unittest.TestCase):
+    def setUp(self):
+        self.heads = [
+            array_to_binary([3, 5, 6, 9, 17, 23]),
+            array_to_binary([0, 2, 5, 6, 11, 45, 101]),
+        ]
+
+    def test_list_of_depths(self):
+        for head in self.heads:
+            depth_nodes = []
+            list_of_depths(head, depth_nodes)
+            for depth, nodes in enumerate(depth_nodes):
+                print(depth, nodes)
+
+
+if __name__ == "__main__":
+    unittest.main()
